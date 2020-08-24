8bff902e827279730cafadf72be56cc8d17ddda0
Admin <akshaynaramshettiwar143@gmail.com>
2020-08-24 12:03:01 +0530

    new commit

--- src/akshay2/A.java
+++ src/akshay2/A.java
@@ -3,6 +3,6 @@ package akshay2;
 public class A {
 public static void main(String[] args) {
 	
-	System.out.println("akshay Naramshettiwar");
+	System.out.println("Hi, I am Akshay Naramshettiwar");
 }
 }
